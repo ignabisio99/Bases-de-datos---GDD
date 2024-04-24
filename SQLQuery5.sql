228d7602b9f32d12336fadbccb8150f23d6b763b
ignabisio99 <ignacio.bisio8780@gmail.com>
2024-04-24 14:12:58 -0300

    Practica 5

---
+++ SQLQuery5.sql
@@ -0,0 +1,90 @@
+-- EJ 1
+
+SELECT o.customer_num, c.company, o.order_num FROM orders o
+JOIN customer c ON c.customer_num = o.customer_num
+ORDER BY o.customer_num
+
+-- EJ 2
+
+SELECT i.order_num, i.item_num, t.description, i.manu_code, i.quantity, (i.quantity * i.unit_price) as PrecioTotal
+FROM items i
+JOIN product_types t ON t.stock_num = i.stock_num
+WHERE i.order_num = 1004
+
+-- EJ 3
+
+SELECT i.order_num, i.item_num, t.description, i.manu_code, i.quantity, (i.quantity * i.unit_price) as PrecioTotal, m.manu_name
+FROM items i
+JOIN product_types t ON t.stock_num = i.stock_num
+JOIN manufact m ON m.manu_code =i.manu_code
+WHERE i.order_num = 1004
+
+-- EJ 4
+
+SELECT o.order_num, o.customer_num, c.fname, c.lname, c.company FROM orders o
+JOIN customer c ON c.customer_num = o.customer_num
+
+-- EJ 5
+
+SELECT DISTINCT (o.customer_num), c.fname, c.lname, c.company FROM orders o
+JOIN customer c ON c.customer_num = o.customer_num
+
+-- EJ 6
+
+SELECT m.manu_name, p.stock_num, t.description, u.unit, p.unit_price, (p.unit_price *1.2) as PrecioJunio 
+FROM manufact m
+JOIN products p ON p.manu_code = m.manu_code
+JOIN product_types t ON t.stock_num = p.stock_num
+JOIN units u ON u.unit_code = p.unit_code
+
+-- EJ 7
+
+SELECT i.item_num, pt.description, i.quantity, i.quantity * i.unit_price AS PrecioTotal
+FROM items i
+JOIN product_types pt ON i.stock_num = pt.stock_num
+WHERE i.order_num = 1004;
+
+-- EJ 8
+
+SELECT m.manu_name, m.lead_time FROM orders o
+JOIN items i on i.order_num = o.order_num
+JOIN manufact m on m.manu_code = i.manu_code
+WHERE i.order_num = 104
+
+-- EJ 9
+
+SELECT o.order_num, o.order_date, i.item_num, t.description, i.quantity, i.quantity * i.unit_price AS PrecioTotal 
+FROM orders o
+JOIN items i on i.order_num = o.order_num
+JOIN product_types t on t.stock_num = i.stock_num
+
+-- EJ 10
+
+SELECT lname + ', ' + fname, '(' + SUBSTRING(phone,1,3) + ') ' + SUBSTRING(phone,5,12) FROM customer
+ORDER BY 1
+
+-- EJ 11
+
+SELECT o.ship_date, c.lname + ', ' + c.fname, COUNT(o.order_num) FROM orders o
+JOIN customer c ON o.customer_num = c.customer_num
+JOIN state s ON s.state = c.state
+WHERE s.sname = 'California' AND c.zipcode BETWEEN 94000 AND 94100
+GROUP BY o.ship_date, c.lname + ', ' + c.fname
+ORDER BY 1,2
+
+-- EJ 12
+
+SELECT m.manu_name, t.description, i.quantity, i.unit_price * i.quantity AS MontoTotal FROM items i
+JOIN manufact m ON m.manu_code = i.manu_code
+JOIN product_types t ON t.stock_num = i.stock_num
+JOIN orders o ON o.order_num = i.order_num
+WHERE MONTH(o.order_date) = 5 OR MONTH(o.order_date) = 6
+ORDER BY i.unit_price * i.quantity DESC
+
+-- EJ 13
+
+SELECT FORMAT(o.order_date,'yyyy-MM') AS Mes ,SUM(i.quantity) AS Cantidad, SUM(i.quantity * i.unit_price) AS Monto_Total FROM orders o
+JOIN items i on i.order_num = o.order_num
+GROUP BY FORMAT(o.order_date,'yyyy-MM'),i.stock_num
+ORDER BY FORMAT(o.order_date,'yyyy-MM') DESC
+
